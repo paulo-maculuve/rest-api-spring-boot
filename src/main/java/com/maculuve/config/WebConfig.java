82b63c955f804c290f30ce8537179b6e8a8cb455
Paulo Maculuve <75036310+paulo-maculuve@users.noreply.github.com>
2025-01-29 20:49:52 +0200

    .

--- src/main/java/com/maculuve/config/WebConfig.java
+++ src/main/java/com/maculuve/config/WebConfig.java
@@ -1,12 +1,31 @@
 package com.maculuve.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatters:default}")
+    private String corsOriginPatters = "";
+
+
+    
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatters.split(",");
+        registry.addMapping("/**")
+        // .allowedMethods("GET", "POST", "PUT")
+        .allowedMethods("*")
+        .allowedOrigins(allowedOrigins)
+        .allowCredentials(true);
+    }
+
+
 
     @Override
     public void configureContentNegotiation(@SuppressWarnings("null") ContentNegotiationConfigurer configurer) {
